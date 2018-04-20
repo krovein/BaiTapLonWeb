846cb7e63e5ae25f182aa0435a83cbdcd32ccbd5
lamhv <krovein@gmail.com>
2018-04-20 09:16:54 +0700

    thêm quản lý chủ đề

--- BaiTapLonWeb/mtpIndex.Master.designer.cs
+++ BaiTapLonWeb/mtpIndex.Master.designer.cs
@@ -49,6 +49,15 @@ namespace BaiTapLonWeb {
         protected global::System.Web.UI.WebControls.HyperLink itemmenuvietbaimoi;
         
         /// <summary>
+        /// HyperLink2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+        
+        /// <summary>
         /// dangnhap_dangxuat control.
         /// </summary>
         /// <remarks>
---
+++ BaiTapLonWeb/quanlychude.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BaiTapLonWeb
+{
+    public partial class quanlychude : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection cnn = new SqlConnection(ConfigurationManager.ConnectionStrings["strdbmpm"].ToString());
+            cnn.Open();
+            string strcmd = "select * from tblChude";
+            SqlCommand cmd = new SqlCommand(strcmd, cnn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cnn.Close();
+            grdV.DataSource = dt;
+            grdV.DataBind();
+        }
+    }
+}
